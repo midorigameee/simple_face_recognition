465d1d4fe51d0f75104e9fb7916c8d6931b51b13
shin-2D7 <xqbodp727@gmail.com>
2018-11-12 18:35:09 +0900

    Initial commit

---
+++ extract_face_images.py
@@ -0,0 +1,121 @@
+"""
+LOAD_DIR
+    |-manA
+        |-imageA_0
+        |-imageA_1
+        ...
+    |-manB
+        |-imageB_0
+        |-imageB_1
+        ...
+    ...
+
+SAVE_DIR
+    |-manA
+        |-face_imageA_0
+        |-face_imageA_1
+        ...
+    |-manB
+        |-face_imageB_0
+        |-face_imageB_1
+        ...
+    ...
+
+上記のようなディレクトリ構造を持つLOAD_DIRから顔写真を抽出する。
+抽出された顔画像は上記のようなディレクトリ構造を持つSAVE_DIRに保存される
+"""
+
+import cv2
+import os
+import numpy as np
+
+
+FACE_SIZE = None    # None以外ならその大きさにリサイズする
+LOAD_DIR = "original_data"
+SAVE_DIR = "training_data"
+
+# 分類器の指定
+cascade_file = "C:\\workspace_py\\Anaconda3\\envs\\py35\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_alt2.xml"
+cascade = cv2.CascadeClassifier(cascade_file)
+
+
+def detect_maxsize_face(target_image):
+    target_gray = cv2.cvtColor(target_image, cv2.COLOR_BGR2GRAY)
+    face_list = cascade.detectMultiScale(target_gray, minSize=(50, 50))        
+
+    # 検出した顔に印を付ける
+    f_x = 0
+    f_y = 0
+    f_size = 0
+    for (x, y, w, h) in face_list:
+        color = (0, 0, 225)
+        pen_w = 1
+
+        # 一番大きい検出結果を顔とする
+        if w > f_size:
+            f_x = x
+            f_y = y
+            f_size = w
+
+    return len(face_list), f_x, f_y, f_size
+
+
+def extract_face(image, resize=None):
+    f_num, f_x, f_y, f_size = detect_maxsize_face(image)
+    face_image = None
+
+    # 顔があったらその場所を抽出する
+    if f_num > 0:
+        face_image = image[f_y:f_y+f_size, f_x:f_x+f_size]
+
+        # 顔画像をリサイズ
+        if resize is not None:
+            size = (resize, resize)
+            face_image = cv2.resize(face_image, size)
+
+    return face_image
+
+
+def extract_target_multi(target_dir, save_dir):
+    # 対象画像の読み込み
+    class_list = os.listdir(target_dir)
+
+    # 保存用のディレクトリ作成
+    if os.path.exists(save_dir) is not True:
+        os.mkdir(save_dir)
+
+    # クラスごとに画像を読み込む
+    for class_name in class_list:
+        print("\n----- {} -----" .format(class_name))
+
+        # クラスごとに保存用のディレクトリ作成
+        class_save_dir = os.path.join(save_dir, class_name)
+        if os.path.exists(class_save_dir) is not True:
+            os.mkdir(class_save_dir)
+
+        # class内の画像のファイル名をリスト化
+        target_list = os.listdir(os.path.join(target_dir, class_name))
+
+        for idx, target_name in enumerate(target_list):
+            target_path = os.path.join(target_dir, class_name, target_name)
+            target_data = cv2.imread(target_path, cv2.IMREAD_COLOR)
+            target_face = extract_face(target_data)
+
+            if target_face is None:
+                print("[{}]{}:\n=> Faces are not found." .format(idx, target_name))
+            else:
+                print("[{}]{}:\n=> Save a face image." .format(idx, target_name))
+
+                # 顔画像をリサイズ
+                if FACE_SIZE is not None:
+                    size = (FACE_SIZE, FACE_SIZE)
+                    target_face = cv2.resize(target_face, size)
+
+                # 顔画像を保存
+                save_name = "face_image_" + class_name + str(idx) + ".jpg"
+                save_path = os.path.join(class_save_dir, save_name)
+                cv2.imwrite(save_path, target_face)
+
+
+if __name__ == "__main__":
+    extract_target_multi(LOAD_DIR, SAVE_DIR)
---
+++ train_model.py
@@ -0,0 +1,131 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import torchvision
+from torchvision import transforms, datasets
+import os
+
+
+# Device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print("device:", device)
+
+# Hyper parameters
+NUM_EPOCHS = 100
+IMAGE_SIZE = 32
+BATCH_SIZE = 10
+DATASET_DIR = "training_data"
+
+
+"""
+32*32のRGB画像に対応しているCNN
+
+input(3, 32, 32)
+conv1(3, 6, 5)   => (6, 28, 28)
+pool1(2, 2)      => (6, 14, 14)
+conv2(6, 16, 5)  => (16, 10, 10)
+pool2(2, 2)      => (16, 5, 5)
+
+16 * 5 * 5 = 400 => 120
+120 => 84
+84 => 4
+
+この構造はLeNetと呼ばれるCNNの初期モデル
+"""
+class CNN(nn.Module):
+    def __init__(self, num_class):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_class)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def main():
+    # 取り込んだデータに施す処理を指定
+    data_transform = transforms.Compose([
+            transforms.Resize(IMAGE_SIZE),
+            transforms.ToTensor(),
+        ])
+
+    # データセットの読み込み
+    full_dataset = datasets.ImageFolder(root=DATASET_DIR,
+                                        transform=data_transform)
+
+    # 訓練用とテスト用に分割
+    train_size = int(0.8 * len(full_dataset))  
+    test_size = len(full_dataset) - train_size  
+    train_dataset, test_dataset = torch.utils.data.random_split(
+                                        dataset=full_dataset,
+                                        lengths=[train_size, test_size])  
+
+    # DataLoader化
+    train_loader = torch.utils.data.DataLoader(
+                                        dataset=train_dataset,
+                                        batch_size=BATCH_SIZE,
+                                        shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+                                        dataset=test_dataset,
+                                        batch_size=BATCH_SIZE,
+                                        shuffle=False)
+
+    # モデル、損失関数、最適化関数の定義
+    num_class = len(os.listdir(DATASET_DIR))    # class数を取得
+    model = CNN(num_class).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # モデルのトレーニング
+    total_step = len(train_loader)
+    for epoch in range(NUM_EPOCHS):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i+1) % 5 == 0:
+                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                       .format(epoch+1, NUM_EPOCHS, i+1, total_step, loss.item()))
+
+    # モデルのテスト
+    model.eval()  # ネットワークを推論モードに切り替える
+    with torch.no_grad():   # 推論中は勾配の保存を止める
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print('Test Accuracy: {:.3f} %' .format(100 * correct / total))
+
+    # モデルの保存
+    torch.save(model.state_dict(), 'model.ckpt')
+
+
+if __name__ == "__main__":
+    main()
